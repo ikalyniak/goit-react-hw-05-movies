0b235b55b14a036f4015c02572d60e51f06cfc60
Ihor Kalyniak <kalyniak.ihor@gmail.com>
2021-10-02 14:01:32 +0300

    api

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,3 @@ function App() {
 }
 
 export default App;
-
-//API Key: b3f7842d4247fd44ff025435e97fa37b
-//https://www.themoviedb.org/talk/6150f01467dcc90043dd1173?page=1#6150f01467dcc90043dd1176
---
+++ src/services/movie-API.js
@@ -0,0 +1,39 @@
+const API_KEY = 'b3f7842d4247fd44ff025435e97fa37b'; //https://www.themoviedb.org/talk/6150f01467dcc90043dd1173?page=1#6150f01467dcc90043dd1176
+const BASE_URL = 'https://api.themoviedb.org/3/';
+
+async function fetchWithErrorHandling(url = '') {
+  const response = await fetch(url);
+  return response.ok
+    ? await response.json()
+    : Promise.reject(new Error('Not found'));
+}
+
+export function fetchTrending() {
+  return fetchWithErrorHandling(
+    `${BASE_URL}trending/all/day?api_key=${API_KEY}`,
+  );
+}
+
+export function fetchSearch() {
+  return fetchWithErrorHandling(
+    `${BASE_URL}search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false`,
+  );
+}
+
+export function fetchMovieById(movieId) {
+  return fetchWithErrorHandling(
+    `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`,
+  );
+}
+
+export function fetchCast(movieId) {
+  return fetchWithErrorHandling(
+    `${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`,
+  );
+}
+
+export function fetchReviews(movieId) {
+  return fetchWithErrorHandling(
+    `${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`,
+  );
+}
