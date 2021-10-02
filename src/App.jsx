62e8d6ceeda02e2ed3b71005e3d1cfeeff835362
Ihor Kalyniak <kalyniak.ihor@gmail.com>
2021-10-02 13:05:03 +0300

    Navigation

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import AppBar from './components/AppBar/AppBar';
+import Container from './components/Container/Container';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <AppBar />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
+
+//API Key: b3f7842d4247fd44ff025435e97fa37b
+//https://www.themoviedb.org/talk/6150f01467dcc90043dd1173?page=1#6150f01467dcc90043dd1176
