1909c30c5651e5a65f4980579d4b3a176a000949
Ihor Kalyniak <kalyniak.ihor@gmail.com>
2021-10-02 13:21:26 +0300

    views

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Switch, Route } from 'react-router-dom';
+
 import AppBar from './components/AppBar/AppBar';
 import Container from './components/Container/Container';
+import HomePage from './views/HomePage';
+import MoviesPage from './views/MoviesPage';
+import MovieDetailsPage from './views/MovieDetailsPage';
+import NotFoundView from './views/NotFoundView';
 import './App.css';
 
 function App() {
@@ -7,6 +13,23 @@ function App() {
     <div className="App">
       <Container>
         <AppBar />
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+
+          <Route path="/movies">
+            <MoviesPage />
+          </Route>
+
+          <Route path="/movies/:movieId" exact>
+            <MovieDetailsPage />
+          </Route>
+
+          <Route>
+            <NotFoundView />
+          </Route>
+        </Switch>
       </Container>
     </div>
   );
